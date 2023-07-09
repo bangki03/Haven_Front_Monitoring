bf266dd5d3627be539c57babe0b76efbee1af1fb
Bangki <bangki03@gmail.com>
2023-07-09 23:43:49 +0900

    Version : A.05_rev2 Date : 23.07.09 23:50
    <Revisions>
    
    [LoginView]
     - 버전명 마우스 올릴 시, 변경사항 표시 기능 추가 (배포 실수로 인한 재배포)
    
    [MyPageView]
     -
    
    [ProjectView]
     -
    
    [MonitoringView]
     -

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -10,14 +10,14 @@
         style="height: 58.9px; width: 176px; padding: 0;"
       />
       <p style="margin-left: auto; margin-right:0.5em; height: 1.5rem; margin-bottom: 0; cursor: pointer;" @mouseenter="showrevision = true" @mouseleave="showrevision = false">
-        Version : A.05 / Relase Date : 23.07.09 23:30</p>
+        Version : A.05_rev2 / Relase Date : 23.07.09 23:50</p>
       <div v-if=showrevision class="revision">
         <br>
-        Version : A.05<br>
-        Relase Date : 23.07.09 23:30 <br>
+        Version : A.05_rev2<br>
+        Relase Date : 23.07.09 23:50 <br>
         <br>
         [LoginView]<br>
-        - 버전명 마우스 올릴 시, 변경사항 표시 기능 추가<br>
+        - 버전명 마우스 올릴 시, 변경사항 표시 기능 추가 (배포 실수로 인한 재배포)<br>
         <br>
       </div>
     </div>
@@ -37,6 +37,7 @@ export default{
   },
   data() {
     return{
+      showrevision: false,
     }
   },
 
@@ -63,7 +64,22 @@ export default{
 </script>
 
 <style scoped>
+.revision {
+  position: absolute;
+  top: 2.0rem;
+  left: 80%;
+  background-color: rgba(255,255,255,0.2);
+  /* z-index: 999; */
+  padding: 0.5em;
+  border-radius: 4px;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
 
+  text-align: left;
+  /* display:flex;
+  flex-direction: column;
+  align-items: flex-start; */
+  /* justify-content: flex-start; */
+}
 
 
 </style>
